ccb1b826ee8cf73d4e269b8fbab9581b25d5cee8
Norman Jarvis <norman.jarvis@evernym.com>
2019-09-30 11:56:31 -0600

    Added handler to have a remote logger for our mobile app

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express')
+const fs = require('fs')
+const url = require('url')
+
 const app = express()
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/connectmelog', (req, res) => {
+
+  var body = '';
+  filePath = __dirname + '/connectmelogs/connectmelog.txt';
+  req.on('data', function(data) {
+      body += data;
+  });
+
+  req.on('end', function () {
+      fs.appendFile(filePath, new Date().toUTCString() +  " :: " + body + '\n', function() {
+        res.send('Logged it');
+      });
+  });
+
+})
+
 app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
